a14dca81419134b743a6ff36abbb1dcc758e6638
Ashley Baldwin-Hunter & Ricky Thomas <pair+ABaldwinHunter.ricky-thomas@devbootcamp.com>
2015-01-05 12:02:21 -0500

    Creates Game constructor with function to create nested array board

---
+++ js/game.js
@@ -0,0 +1,26 @@
+function Game(startBoard) = {
+  this.completed = false;
+  this.board = makeBoard(startBoard);
+};
+
+Game.prototype.makeBoard = function(stringBoard){
+  if (stringBoard.length !== 16){
+    alert("Starting boards must be 16 digits long, you silly hobbit!");
+    return false
+  } else {
+    var board = [];
+    for (var i = 0; i < stringBoard.length; i+=4) {
+      var array = [];
+      for (var j = i; j < i+4; j++) {
+      array.push(parseInt(stringBoard[j]));
+      }
+    board.push(array);
+    }
+  }
+  return board;
+};
+
+
+// Game.prototype.toString = function(){
+
+// };
